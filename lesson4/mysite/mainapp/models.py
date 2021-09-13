fc9206715968b199e599ed46e78aad60478ca7ee
Sergey Pedchenko <pedchenko.sergey@gmail.com>
2021-09-13 22:08:29 +0300

    Created Models for goods and superuser

--- lesson4/mysite/mainapp/models.py
+++ lesson4/mysite/mainapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class GoodModel(models.Model):
+    title = models.CharField(
+        verbose_name="Название",
+        max_length=255
+    )
+    created_date = models.DateTimeField(
+        verbose_name="Дата Поступления",
+        auto_created=True,
+        auto_now_add=True
+    )
+
+    price = models.PositiveIntegerField(
+        verbose_name="Цена",
+        default=0
+    )
+
+    vendor = models.CharField(
+        verbose_name="Поставщик",
+        max_length=255
+    )
+
+    def __str__(self):
+        return f"{self.title} | {self.vendor}"
+
+    class Meta:
+        verbose_name = "Карточка товара"
+        verbose_name_plural = "Карточки товаров"
