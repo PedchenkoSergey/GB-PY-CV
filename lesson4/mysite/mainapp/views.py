ffaaf0245669edefc17368073f374c4a3962b465
Sergey Pedchenko <pedchenko.sergey@gmail.com>
2021-09-14 01:37:18 +0300

    Created base solution for DZ5

--- lesson4/mysite/mainapp/views.py
+++ lesson4/mysite/mainapp/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template.loader import render_to_string
 from django.urls import reverse
 from django.views.generic import ListView
 from .models import GoodModel
@@ -20,9 +21,24 @@ class ItemsListView(ListView):
 def good_add(request):
     if request.method == "POST":
         form = ProductEditForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('mainapp:goods-list'))
+        return save_good_form(request, form, 'mainapp/good_create.html')
     else:
         form = ProductEditForm()
-        return render(request, 'mainapp/good_create.html', {'form': form})
+        return save_good_form(request, form, 'mainapp/good_create.html')
+
+
+def save_good_form(request, form, template_name):
+    data = dict()
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            data['form_is_valid'] = True
+            goods = GoodModel.objects.all()
+            data['html_good_list'] = render_to_string('mainapp/goods_list.html', {
+                'object_list': goods
+            })
+        else:
+            data['form_is_valid'] = False
+    context = {'form': form}
+    data['html_form'] = render_to_string(template_name, context, request=request)
+    return JsonResponse(data)
